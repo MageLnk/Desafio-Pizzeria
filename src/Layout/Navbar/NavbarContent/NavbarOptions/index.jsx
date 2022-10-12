70f5f1424744697cbe7ce1067108192a75e72a30
MageLink <mage.link.zhezard@gmail.com>
2022-10-12 19:57:52 -0300

    Navbar options partialy complete

--- src/Layout/Navbar/NavbarContent/NavbarOptions/index.jsx
+++ src/Layout/Navbar/NavbarContent/NavbarOptions/index.jsx
@@ -1,11 +1,32 @@
+// Style
+import { BsLayoutTextSidebarReverse } from "react-icons/bs";
+import { GiHotMeal } from "react-icons/gi";
+import { GoPerson } from "react-icons/go";
+// App
 const NavbarOptions = () => {
   return (
     <div className="navbar-options">
-      <span>Menú</span>
-      <span>Papa puntos</span>
-      <span>Mis pedidos</span>
-      <span>Locales</span>
-      <span>Ingresar</span>
+      <div className="navbar-option-menu">
+        <span>
+          <GiHotMeal style={{ fontSize: "18px" }} /> Menú
+        </span>
+      </div>
+      <div className="navbar-option-container">
+        <span>Pizza puntos</span>
+      </div>
+      <div className="navbar-option-my-asks">
+        <span>
+          <BsLayoutTextSidebarReverse /> Mis pedidos
+        </span>
+      </div>
+      <div className="navbar-option-container">
+        <span>Locales</span>
+      </div>
+      <div className="navbar-option-enter">
+        <span>
+          <GoPerson className="navbar-option-enter-icon" /> Ingresar
+        </span>
+      </div>
     </div>
   );
 };
