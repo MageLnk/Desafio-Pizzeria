d2093bdeb331f2cbf3959f89323dda58e09e13c4
MageLink <mage.link.zhezard@gmail.com>
2022-10-12 22:46:46 -0300

    Footer done

--- src/Layout/Footer/FooterContent/index.jsx
+++ src/Layout/Footer/FooterContent/index.jsx
@@ -1,8 +1,16 @@
 // Style
+import { BiRegistered } from "react-icons/bi";
 import "./FooterContent.css";
 // App
 const FooterContent = () => {
-  return <div className="footer-content-container">FooterContent</div>;
+  return (
+    <div className="footer-content-container">
+      <span>
+        Página creada por poderoso MageLink a petición de D.Latam para una prueba de React. Manejo de Browser Router II
+        y otras poderosas virtudes <BiRegistered />
+      </span>
+    </div>
+  );
 };
 
 export default FooterContent;
