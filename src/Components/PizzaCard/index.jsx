50a7118c04b3c4b502db629bfd5cf37ccace5578
MageLink <mage.link.zhezard@gmail.com>
2022-10-18 20:48:54 -0300

    Basic Layout for home is completed

--- src/Components/PizzaCard/index.jsx
+++ src/Components/PizzaCard/index.jsx
@@ -2,20 +2,25 @@
 import "./PizzaCard.css";
 import { Card } from "antd";
 import { Link } from "react-router-dom";
+// Utilities
+import { mayusFirstLetter } from "../../utilities";
 // App
 const PizzaCard = ({ pizzaInfo }) => {
   console.log("AFDS", pizzaInfo);
   return (
     <div className="card-pizza-container">
-      <Link to={`/pizza/${pizzaInfo.id}`}>
-        <Card hoverable cover={<img alt="pizza" src={pizzaInfo.img} />}>
-          <h4>{pizzaInfo.name}</h4>
-          <div className="card-pizza-options">
-            <span>Algo</span>
-            <span>Algo</span>
-          </div>
-        </Card>
-      </Link>
+      <Card hoverable style={{ cursor: "default" }} cover={<img alt="pizza" src={pizzaInfo.img} />}>
+        <h4>
+          {mayusFirstLetter(pizzaInfo.name)} | Precio: {pizzaInfo.price}
+        </h4>
+        <div className="card-pizza-options">
+          <Link to={`/pizza/${pizzaInfo.id}`}>
+            <button>Más info</button>
+          </Link>
+
+          <button>Pedir</button>
+        </div>
+      </Card>
     </div>
   );
 };
---
+++ src/utilities/index.js
@@ -0,0 +1,1 @@
+export { default as mayusFirstLetter } from "./mayusFirstLetter";
