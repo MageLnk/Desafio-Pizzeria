903d6a0307fa5cd826adbfa429b39b90831c845b
MageLink <mage.link.zhezard@gmail.com>
2022-10-18 21:31:19 -0300

    Changes of pages with still in working progress

--- src/Components/ContentComponents/PizzaPointsContent/index.jsx
+++ src/Components/ContentComponents/PizzaPointsContent/index.jsx
@@ -2,7 +2,15 @@
 import "./PizzaPointsContent.css";
 // App
 const PizzaPointsContent = () => {
-  return <div className="pizza-points-content-container">PizzaPointsContent</div>;
+  return (
+    <div
+      className="pizza-points-content-container"
+      style={{ display: "flex", height: "100%", justifyContent: "center", alignItems: "center" }}
+    >
+      Basándome en la página de Papa Jhons, acá es una vista para las promociones de "PizzaPoints",
+      que me imagino que es unas promociones para los clientes frecuentes
+    </div>
+  );
 };
 
 export default PizzaPointsContent;
